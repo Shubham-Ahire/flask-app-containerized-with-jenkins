0461721007b6982b2bb0c5dff559a80a5f30dae4
Shubham Ahire <104445367+Shubham-Ahire@users.noreply.github.com>
2025-09-24 23:22:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    return "<h1>Hello, this is Shubham Ahire!</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
